3b73f7816295606be805c03b38e90feee061e438
vboxuser <vboxuser@Ubuntu22.04.myguest.virtualbox.org>
2025-06-16 00:07:34 -0700

    Custom messages work. Example usage in planner

--- ROS/ws/src/kraken/setup.py
+++ ROS/ws/src/kraken/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'custom'],
     zip_safe=True,
     maintainer='vboxuser',
     maintainer_email='vboxuser@todo.todo',
