9650b3f09654f027ae3980e20a1bd777869dbaa1
Ethan Dean <ethanjamesdean@gmail.com>
2025-07-29 22:54:13 -0700

    Added basic xyz position estimation with simulation

--- ROS/ws/src/kraken/kraken/state_estimator.py
+++ ROS/ws/src/kraken/kraken/state_estimator.py
@@ -1,15 +1,41 @@
 import rclpy
 from rclpy.node import Node
+import sys
+import time
+
+sys.path.append("/home/vboxuser/kraken-nano/ROS/ws/src/kraken/kraken/include")
+
+from simulation import Simulation
 
 class StateEstimator(Node):
 
     def __init__(self):
         super().__init__('state_estimator')
-        timer_period = 0.5  # seconds
+        timer_period = 0.005  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.sim = Simulation(self)
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.current = time.time()
+        self.prev = time.time()
 
     def timer_callback(self):
-        self.get_logger().info('State Estimator Running')
+        depth = self.sim.get_depth()
+        orient = self.sim.get_orientation()
+
+        self.current = time.time()
+        delta = self.current - self.prev
+        self.prev = time.time()
+        accel = self.sim.get_acceleration()
+        
+        if depth is not None:
+                self.z = depth
+        if accel is not None:
+                self.x += (delta ** 2 / 2) * accel.x
+                self.y += (delta ** 2 / 2) * accel.y
+                
+        self.get_logger().info(str(self.x) + ', ' + str(self.y) + ', ' + str(self.z))
 
 
 def main(args=None):
